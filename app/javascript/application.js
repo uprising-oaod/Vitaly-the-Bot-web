2cfe6ef31fcad581f201b9d9082f4ceed103c4fb
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-01-28 19:30:26 +0700

    #1 Добавление скриптов js бутстрапа и зависимостей

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Add Bootstrap dependencies and Bootstrap
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
+// While bootstrap-sprockets provides individual Bootstrap components for ease of debugging,
+//     you may alternatively require the concatenated bootstrap for faster compilation:
+//
+// // require jquery3
+// // require popper
+// // require bootstrap
