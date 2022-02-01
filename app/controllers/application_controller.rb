0a9463c8f10f26c72b73c223d8d8c03ee3851f53
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-02-01 22:52:55 +0700

    #6 Примеры использования Подробнее см.здесь https://github.com/paper-trail-gem/paper_trail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  # If your controllers have a current_user method,
+  # you can easily track who is responsible for changes by adding a controller callback.
+  before_action :set_paper_trail_whodunnit
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+  
+  # Add has_paper_trail to the models you want to track
+  has_paper_trail
 end
