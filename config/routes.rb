03252c6fa9487da6be5a9d596cde6a59838c3a9a
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-02-02 23:00:24 +0700

    #3 Определяем эндпоинты в форме маршрутов

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,26 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
 
+  # Определяем эндпоинты в форме маршрутов
+  scope '/api' do
+    scope '/v1' do
+      scope '/projects' do
+        get '/' => 'api_projects#index'
+        post '/' => 'api_projects#create'
+        scope '/:name' do
+          get '/' => 'api_projects#show'
+          put '/' => 'api_projects#update'
+          scope '/todos' do
+            get '/' => 'api_todos#index'
+            post '/' => 'api_todos#create'
+            scope '/:todo_name' do
+              get '/' => 'api_todos#show'
+              put '/' => 'api_todos#update'
+            end
+          end
+        end
+      end
+    end
+  end
+
 end
