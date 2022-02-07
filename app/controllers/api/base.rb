4242d52200fabde33e9bf42db2c4c7129cf956c0
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-02-07 22:13:23 +0700

    #10 Подключение swagger Будет доступно по адресу http://localhost:3000/swagger_doc На этот адрес можно настроить SwaggerUI

--- app/controllers/api/base.rb
+++ app/controllers/api/base.rb
@@ -1,6 +1,11 @@
+require "grape-swagger"
+
 module API
   class Base < Grape::API
     mount API::V1::Base  # Ключевое слово mount говорит приложению, что
     # здесь присутствует другое приложение (в нашем случае Grape API).
+    #
+    # Подключаем swagger
+    add_swagger_documentation
   end
 end
