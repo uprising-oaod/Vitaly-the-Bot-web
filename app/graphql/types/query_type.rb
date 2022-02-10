8a7aab775610952aef99432dfbc27212abe54651
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-02-10 22:48:17 +0700

    #4 создание GraphQL Query Resolver для обработки типа LinkType Для проверки можно использовать адрес http://localhost:3000/graphiql

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,14 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # queries are just represented as fields
+    # `all_links` is automatically camelcased to `allLinks`
+    field :all_links, [LinkType], null: false
+
+    # this method is invoked, when `all_link` fields is being resolved
+    def all_links
+      Link.all
     end
+
   end
 end
