4613ff97d735772f2fc0a4dcd8842bec20937b66
butuhanov <36451784+butuhanov@users.noreply.github.com>
2022-02-13 21:50:57 +0700

    #16 генерация документации и добавление примера в контроллере Welcome генерируем документацию командой > yard

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,14 @@
 class WelcomeController < ApplicationController
   before_action :authenticate_user!
+
+  # Пример документации
+  # @param welcome_id [Integer] id приветствия
+  # @note Это пример документации
+  # @example
+  #   welcome(2)
+  # @return [Boolean] отображение приветствия
+  # @see User
+  def welcome
+
+  end
 end
